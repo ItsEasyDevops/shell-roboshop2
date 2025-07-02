579afe48fb90517875dec180df912cf36902d3a8
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-01 22:06:34 -0400

    Updating roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for instance in ${INSTANCES[@]}
 do
-    if [ "$instance" -eq "frontend" ]; then
+    if [ "$instance" = "frontend" ]; 
+    then
         tag_value="frontend"
     else
         tag_value="$instance"
@@ -19,9 +20,9 @@ do
         --image-id ami-09c813fb71547fc4f \
         --instance-type t2.micro \
         --security-group-ids sg-0aa27df1889c9933e \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$tag_value'}]' --query "Instances[0].InstanceId" --output text)
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$tag_value}]' --query "Instances[0].InstanceId" --output text)
 
-    if [ $instance eq "frontend" ]
+    if [ $instance = "frontend" ];
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
@@ -30,7 +31,7 @@ do
 
     echo "$instance instance created with IP Address: $IP"
 
-    if [ $instance -eq "frontend" ]
+    if [ $instance = "frontend" ];
     then
         record_name=$DOMAIN_NAME
     else
