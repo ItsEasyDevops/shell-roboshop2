aabe045e872a73eac767b53735677d6f0274fd6a
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-15 21:12:57 -0400

    shipping file

--- shipping.sh
+++ shipping.sh
@@ -17,42 +17,29 @@ mkdir -p $LOGS_FOLDER
 echo -e "Script Name: $SCRIPT_NAME executing at $(date)" | tee -a $LOG_FILE
 
 # To check if the user has root privileges
-# If the user ID is not 0, it means the script is not being run as root
-# If the user ID is 0, it means the script is being run as root
-if [ $USERID -ne 0 ];
-then
+if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #Giving other that 0 to 127 for error
+    exit 1
 else
     echo -e "$G You are running this script with root access $N" | tee -a $LOG_FILE
 fi
 
-
-# VALIDATE function to check the exit status of the last command executed
-# $1 - Exit status of the last command
-# $2 - Name of the service being installed
-# Usage: VALIDATE $? <service_name>
-# Example: VALIDATE $? mongodb
-# This function will print a success or failure message based on the exit status
-# and log it to the specified log file.
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
         echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
-        exit 1 #Giving other that 0 to 127 for error
+        exit 1
     fi
 }
 
-# Disable Maven module
+# Install Maven
 dnf install maven -y &>>$LOG_FILE
 VALIDATE $? "Maven Installation"
 
-# Add roboshop user
+# Add roboshop user if not exists
 id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ];
-then
+if [ $? -ne 0 ]; then
     echo -e "$Y roboshop user does not exist, creating it now... $N" | tee -a $LOG_FILE
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "roboshop user creation"
@@ -64,69 +51,71 @@ fi
 mkdir -p $SCRIPT_DIR/app &>>$LOG_FILE
 
 # Download the shipping service code
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip    &>>$LOG_FILE
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
 VALIDATE $? "Shipping Code Download"
 
-# Unzip the downloaded code
+# Clean app directory and unzip
 rm -rf $SCRIPT_DIR/app/* &>>$LOG_FILE
-cd $SCRIPT_DIR/app 
-unzip /tmp/shipping.zip
+cd $SCRIPT_DIR/app
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "Shipping Code Extraction"
 
-# Preserve the db folder for schema loading later
+# Backup db files
+rm -rf /tmp/db-backup
+mkdir -p /tmp/db-backup
 cp -r db/* /tmp/db-backup
+VALIDATE $? "DB Files Backup"
 
-# Mvn clean package
-mvn clean package   &>>$LOG_FILE
+# Maven build
+mvn clean package &>>$LOG_FILE
 VALIDATE $? "Maven Package Creation"
 
-# Move the jar file to the app directory
+# Move jar file
 mv $SCRIPT_DIR/app/target/shipping-1.0.jar $SCRIPT_DIR/app/shipping.jar
 
-# Restore the db directory
+# Restore db files
+rm -rf $SCRIPT_DIR/app/db
 mkdir -p $SCRIPT_DIR/app/db
 cp -r /tmp/db-backup/* $SCRIPT_DIR/app/db
+VALIDATE $? "DB Files Restore"
 
+# Copy service file
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
 VALIDATE $? "Shipping Service Copy"
 
-# systemctl daemon-reload
+# Reload systemd
 systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "Systemd Daemon Reload"
 
-# Enable the shipping service
+# Enable and start shipping service
 systemctl enable shipping &>>$LOG_FILE
 VALIDATE $? "Shipping Service Enable"
 
-# Start the shipping service
-systemctl start shipping &>>$LOG_FILE
+systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Shipping Service Start"
 
-# We need to load the schema. To load schema we need to install mysql client.
+# Install MySQL client
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "MySQL Client Installation"
 
-# Check if the shipping schema already exists'
+# Check if shipping schema exists
 echo -e "$Y Checking if shipping schema already exists... $N" | tee -a $LOG_FILE
 mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 
-if [ $? -ne 0 ];
-then
-    # Load the shipping schema
-    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < $SCRIPT_DIR/app/db/schema.sql &>>$LOG_FILE
     VALIDATE $? "Shipping Schema Load"
 
-    # Create the application user for shipping
-    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/app-user.sql    &>>$LOG_FILE
+    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < $SCRIPT_DIR/app/db/app-user.sql &>>$LOG_FILE
     VALIDATE $? "Shipping App User Creation"
 
-    # Load the master data for shipping
-    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.easydevops.fun -uroot -pRoboShop@1 < $SCRIPT_DIR/app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Shipping Master Data Load"
 else
     echo -e "$G Shipping schema already exists, skipping schema load... SKIPPING $N" | tee -a $LOG_FILE
-fi  
+fi
 
-# Restart the shipping service to apply changes
+# Restart shipping service to apply changes
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Shipping Service Restart"
 
