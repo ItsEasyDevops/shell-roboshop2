47b2fe414a9f3874c33fa0c8eee6e52ee42fd991
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-19 22:53:23 -0400

    Updating frontend files

--- frontend.sh
+++ frontend.sh
@@ -1,48 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
+source ./common.sh
+app_name="frontend"
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[32m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$(pwd)
-
-mkdir -p $LOGS_FOLDER
-echo -e "Script Name: $SCRIPT_NAME executing at $(date)" | tee -a $LOG_FILE
-
-# To check if the user has root privileges
-# If the user ID is not 0, it means the script is not being run as root
-# If the user ID is 0, it means the script is being run as root
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 # Giving other than 0 to 127 for error
-else
-    echo -e "$G You are running this script with root access $N" | tee -a $LOG_FILE
-fi
-
-# VALIDATE function to check the exit status of the last command executed
-# $1 - Exit status of the last command
-# $2 - Name of the service being installed
-# Usage: VALIDATE $? <service_name>
-# Example: VALIDATE $? mongodb
-# This function will print a success or failure message based on the exit status
-# and log it to the specified log file.
-VALIDATE() {
-    if [ $1 -eq 0 ]; 
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
-        exit 1 # Giving other than 0 to 127 for error
-    fi
-}
+check_root
 
 # Disable Nginx module
 dnf module disable nginx -y &>>$LOG_FILE
@@ -91,9 +52,7 @@ VALIDATE $? "Copying custom nginx configuration file"
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting Nginx Service"
 
-END_TIME=$(date +%s)
-EXECUTION_TIME=$((END_TIME - START_TIME))
-echo -e "$G Script executed in $EXECUTION_TIME seconds $N" | tee -a $LOG_FILE
+print_time
 
 
 
