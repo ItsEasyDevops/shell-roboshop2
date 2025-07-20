796727c378e6216f6a150e5e1be520f602c9c379
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-19 22:50:27 -0400

    Updating user cart and redis files

--- user.sh
+++ user.sh
@@ -1,106 +1,21 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$(pwd)
-
-mkdir -p $LOGS_FOLDER
-echo -e "Script Name: $SCRIPT_NAME executing at $(date)" | tee -a $LOG_FILE
-
-# To check if the user has root privileges
-# If the user ID is not 0, it means the script is not being run as root
-# If the user ID is 0, it means the script is being run as root
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #Giving other that 0 to 127 for error
-else
-    echo -e "$G You are running this script with root access $N" | tee -a $LOG_FILE
-fi
+source ./common.sh
+app_name="user"
 
 
-# VALIDATE function to check the exit status of the last command executed
-# $1 - Exit status of the last command
-# $2 - Name of the service being installed
-# Usage: VALIDATE $? <service_name>
-# Example: VALIDATE $? mongodb
-# This function will print a success or failure message based on the exit status
-# and log it to the specified log file.
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
-        exit 1 #Giving other that 0 to 127 for error
-    fi
-}
+# Check if the script is run as root
+check_root
 
-# Disable NodeJS module
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "NodeJS Module Disable"
+# Application Setup
+app_setup
 
-# Enable NodeJS 20 repo file
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "NodeJS 20 Module Enable"
+# NodeJS Setup
+nodejs_setup
 
-# Install NodeJS
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "NodeJS Installation"
+# Setup the application
+system_setup
 
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ];
-then
-    echo -e "$Y roboshop user does not exist, creating it now... $N" | tee -a $LOG_FILE
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "roboshop user creation"
-else
-    echo -e "$G roboshop user already exists, skipping creation... SKIPPING $N" | tee -a $LOG_FILE
-fi
-
-mkdir -p /app &>>$LOG_FILE
-VALIDATE $? "Application Directory Creation"
-
-# Download the user application code
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip  &>>$LOG_FILE
-VALIDATE $? "User Application Download"  
-
-rm -rf /app/* &>>$LOG_FILE
-
-# Unzip the user application code
-cd /app 
-unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "User Application Unzip"
-
-# Install NodeJS dependencies
-npm install &>>$LOG_FILE
-VALIDATE $? "User Application Dependencies Installation"
-
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
-VALIDATE $? "User Service File Copy"
-
-# Reload systemd to recognize the new service
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Systemd Daemon Reload"
-
-# Enable User Service
-systemctl enable user &>>$LOG_FILE
-VALIDATE $? "User Service Enable"
-
-# Start User Service
-systemctl start user &>>$LOG_FILE
-VALIDATE $? "User Service Start"
+print_time
 
 
-END_TIME=$(date +%s)
-EXECUTION_TIME=$((END_TIME - START_TIME))
-echo -e "$G Script executed in $EXECUTION_TIME seconds $N" | tee -a $LOG_FILE
