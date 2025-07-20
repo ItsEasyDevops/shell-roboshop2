769318d42c6958e15de4a43aa6903b470bb52876
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-19 22:24:06 -0400

    Updating mongodb and catalogue code

--- common.sh
+++ common.sh
@@ -55,3 +55,77 @@ print_time(){
     EXECUTION_TIME=$(($END_TIME - $START_TIME))
     echo -e "$G Script executed in $EXECUTION_TIME seconds $N" | tee -a $LOG_FILE
 }
+
+
+nodejs_setup(){
+    # NodeJS Installation Script
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "NodeJS Module Disable"
+
+    # Download NodeJS 20 repo file
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "NodeJS 20 Module Enable"
+
+    # Install NodeJS
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "NodeJS Installation"
+
+    # Install NodeJS dependencies
+    npm install &&>>$LOG_FILE
+    VALIDATE $? "Catalogue application dependencies installation"
+}
+
+
+app_setup(){
+    # Create roboshop user
+    # This user will be used to run the application
+    # It is a system user, meaning it does not have a home directory or shell access
+    # The user is created with no login shell and no home directory
+    # This is a security measure to prevent unauthorized access
+
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ];
+    then
+        echo -e "$Y roboshop user does not exist, creating it now... $N" | tee -a $LOG_FILE
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "roboshop user creation"
+    else
+        echo -e "$G roboshop user already exists, skipping creation... SKIPPING $N" | tee -a $LOG_FILE
+    fi
+
+    # Create application directory
+    mkdir -p /app &>>$LOG_FILE
+    VALIDATE $? "Application directory creation"
+
+    # Download the application code
+    # The application code is downloaded from a remote server
+    # The code is downloaded as a zip file and extracted to the /app directory
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "$app_name application code download"
+
+    # Extract the application code
+    # The zip file is extracted to the /app directory
+    rm -rf /app/* &>>$LOG_FILE
+    cd /app &>>$LOG_FILE
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "$app_name application code download and extraction"
+}
+
+
+system_setup(){
+    # Copy the systemd service file for the catalogue service
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service &>>$LOG_FILE
+    VALIDATE $? "$app_name service file copy"
+
+    # Reload systemd to recognize the new service
+    systemctl daemon-reload &>>$LOG_FILE
+    VALIDATE $? "Systemd daemon reload"
+
+    # Start the catalogue service
+    systemctl enable catalogue &>>$LOG_FILE
+    VALIDATE $? "Catalogue service enable"
+
+    # Start the catalogue service
+    systemctl start catalogue &>>$LOG_FILE
+    VALIDATE $? "Catalogue service start"
+}
